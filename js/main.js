8198d8d9ba460b098c4d42e399decec74a26270a
IlyaVolchenko <volchenko.ilya7@gmail.com>
2021-10-30 17:58:37 +0800

    portfolio

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 $(function(){
   
+   $('.portfolio__slider').slick({
+     dots: false,
+     prevArrow: '<button class="slider__prev-btn"><img src="images/arrows-prev.png" alt="prev"></button>',
+     nextArrow: '<button class="slider__next-btn"><img src="images/arrows-next.png" alt="next"></button>'
+   });
+
 });
